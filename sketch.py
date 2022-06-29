660d2d613e4da07b04eb546feb464cc0ebe2c69f
Surya152002 <108388181+Surya152002@users.noreply.github.com>
2022-06-29 21:21:26 +0530

    Add files via upload

---
+++ sketch.py
@@ -0,0 +1,12 @@
+import cv2
+img_location = 'C:/surya/'
+filename = 'ss1.jpg'
+img = cv2.imread(img_location+filename)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+inverted_gray_image = 255 - gray_image
+blurred_img = cv2.GaussianBlur(inverted_gray_image, (21,21),0)
+inverted_blurred_img = 255 - blurred_img
+pencil_sketch_IMG = cv2.divide(gray_image, inverted_blurred_img, scale = 200.0)
+cv2.imshow('behfv4hv',pencil_sketch_IMG)
+cv2.waitKey(0)
+print("hi")
